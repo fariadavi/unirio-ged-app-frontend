8ea6bacc7f01bfa68df695ec3d1fb0e49749697b
Davi Faria <d.a.faria@gmail.com>
2023-06-25 14:56:59 -0300

    disable react dev tools on prod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './i18n'
 import App from './components/App.js'
+import './i18n'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools' 
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM.render(
 	<React.StrictMode>
