c6087356a254975cfd684427d69034c300a9dd0d
Davi Faria <d.a.faria@gmail.com>
2023-12-04 01:34:55 -0300

    add spinner to categories tree data loading and row actions

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -5,7 +5,8 @@ import rq from '../../services/api'
 import TreeView from 'react-accessible-treeview'
 import { Form } from 'react-bootstrap'
 import { Icon } from '../util/CustomIcon'
-import { faCheckCircle, faChevronDown, faChevronRight, faCircle, faPen, faPlusCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle, faChevronDown, faChevronRight, faCircle, faCircleNotch, faPen, faPlusCircle, faTrash } from '@fortawesome/free-solid-svg-icons'
 import '../../style/categories/Categories.css'
 
 const useFocus = () => {
@@ -37,6 +38,34 @@ const ActionBtn = ({ className = '', icon, onClick, tooltip }) =>
         />
     </span>
 
+const Actions = ({ element, isExpanded, savingElements, addNewCategoryToList, editCategory, deleteCategory }) => {
+    const { t } = useTranslation();
+    const [isLoading, setLoading] = useState(false);
+
+    return isLoading || savingElements?.includes(element?.id)
+        ? <FontAwesomeIcon icon={faCircleNotch} className="faSpin" />
+        : <div className="btns actions">
+            <ActionBtn
+                className={element.id < 0 ? 'hide' : ''}
+                icon={faPlusCircle}
+                onClick={() => addNewCategoryToList(element.id, isExpanded)}
+                tooltip={t('customButtons.add.tooltip')}
+            />
+            <ActionBtn
+                className={element.id < 0 ? 'hide' : ''}
+                icon={faPen}
+                onClick={() => editCategory(element.id, element.name)}
+                tooltip={t('customButtons.edit.tooltip')}
+            />
+            <ActionBtn
+                className={element.id < 0 || element.children?.length > 0 ? 'hide' : ''}
+                icon={faTrash}
+                onClick={async () => { setLoading(true); await deleteCategory(element); setLoading(false); }}
+                tooltip={t('customButtons.delete.tooltip')}
+            />
+        </div>
+}
+
 export default function Categories() {
     const { t } = useTranslation();
     const { department } = useContext(UserContext);
@@ -44,15 +73,21 @@ export default function Categories() {
     const [categories, setCategories] = useState([{ "id": 0, "children": [], "parent": null }]);
     const [categoryEdit, setCategoryEdit] = useState(null);
     const [inputRef, setInputFocus] = useFocus();
+    const [isLoading, setLoading] = useState(true);
+    const [savingElements, setSavingElements] = useState([]);
 
     useEffect(() => {
+        setLoading(true);
+
         rq('/categories?numDocs=true', { method: 'GET' })
             .then(res => { if (res.ok) return res.json() })
-            .then(cats => setCategories([
-                { id: 0, name: '', children: [-1, ...cats.filter(c2 => !c2.parent).map(c2 => c2.id)], parent: null },
-                ...cats.map(c => { return { ...c, parent: c.parent || 0, children: cats.filter(c2 => c2.parent === c.id).map(c2 => c2.id) } }),
-                { id: -1, name: ' ', children: [], parent: 0, fixed: true }
-            ]));
+            .then(cats => {
+                setCategories([
+                    { id: 0, name: '', children: [-1, ...cats.filter(c2 => !c2.parent).map(c2 => c2.id)], parent: null },
+                    ...cats.map(c => { return { ...c, parent: c.parent || 0, children: cats.filter(c2 => c2.parent === c.id).map(c2 => c2.id) } }),
+                    { id: -1, name: ' ', children: [], parent: 0, fixed: true }
+                ]); setLoading(false);
+            });
     }, [department]);
 
     const toggleExpand = (id, isExpanded) =>
@@ -140,6 +175,8 @@ export default function Categories() {
 
     const addCategory = (id, name, parent) => {
         if (!name) return;
+            
+        setSavingElements(e => [...e, id]);
 
         rq(`/categories`, {
             method: 'POST',
@@ -152,22 +189,27 @@ export default function Categories() {
             //error feedback
 
             removeCategoryFromList(id, parent);
-        }).then(cat => setCategories(cats =>
+        }).then(cat => { setCategories(cats =>
             cats.map(c => c.id === id
                 ? { ...c, id: cat.id }
                 : c.children.some(c2 => c2 === id)
                     ? { ...c, children: c.children.map(c2 => c2 === id ? cat.id : c2) }
                     : c
             )
-        ));
+        ); setSavingElements(e => e.filter(e2 => e2 !== id)); });
     }
 
-    const updateCategory = (id, name, originalName, originalIndex) =>
+    const updateCategory = (id, name, originalName, originalIndex) => {
+        if (!name) return;
+
+        setSavingElements(e => [...e, id]);
+            
         rq(`/categories/${id}`, {
             method: 'PATCH',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name: name })
         }).then(res => {
+            setSavingElements(e => e.filter(e2 => e2 !== id));
             if (res.ok) return;
 
             console.error(res);
@@ -188,127 +230,114 @@ export default function Categories() {
                         : c
                 ));
         })
+    }
 
-    const deleteCategory = element => {
-        if (element.numDocs > 0) return;
-        if (categories.some(c => c.parent === element.id)) return;
+    const deleteCategory = async element => {
+        if (element.temp || categories.some(c => c.parent === element.id)) return;
 
-        if (!element.temp) {
-            rq(`/categories/${element.id}`, {
-                method: 'DELETE'
-            }).then(res => {
-                if (res.ok) return;
-
+        await rq(`/categories/${element.id}`, {
+            method: 'DELETE'
+        }).then(res => {
+            if (!res.ok) {
                 console.error(res);
                 //error feedback
+                return;
+            }
 
-                addNewCategoryToList(element.parent, true, element);
-            });
-        }
-
-        removeCategoryFromList(element.id, element.parent);
+            removeCategoryFromList(element.id, element.parent);
+        });
     }
 
     return (
         <div className="header-n-table-div categories-page">
             <h1>{t('categories.page.title')}</h1>
-
-            <TreeView
-                data={categories}
-                expandedIds={expandedIds}
-                nodeRenderer={({
-                    element,
-                    level,
-                    isBranch,
-                    isExpanded,
-                    isDisabled,
-                    getNodeProps,
-                    handleExpand,
-                }) => <div {...{
-                    ...getNodeProps({
-                        onClick: e => {
-                            handleExpand(e);
-                            if (isBranch)
-                                toggleExpand(element.id, isExpanded)
-                            else if (categoryEdit)
-                                cancelCategoryEdit(categoryEdit.temp);
-                        }
-                    }),
-                    role: element.fixed ? 'header' : ''
-                }}>
-                        <div className="tree-row">
-                            {
-                                !element.fixed
-                                    ? isBranch
-                                        ? <ArrowIcon isOpen={isExpanded} />
-                                        : <ListIcon />
-                                    : <ActionBtn
-                                        className="icon-wrapper add-category"
-                                        icon={faPlusCircle}
-                                        onClick={() => addNewCategoryToList(0, true)}
-                                        tooltip={t('customButtons.add.tooltip')}
-                                    />
+            {isLoading
+                ? <div className="loading-wrapper">
+                    <FontAwesomeIcon icon={faCircleNotch} className="faSpin" />
+                </div>
+                : <TreeView
+                    data={categories}
+                    expandedIds={expandedIds}
+                    nodeRenderer={({
+                        element,
+                        level,
+                        isBranch,
+                        isExpanded,
+                        isDisabled,
+                        getNodeProps,
+                        handleExpand,
+                    }) => <div {...{
+                        ...getNodeProps({
+                            onClick: e => {
+                                handleExpand(e);
+                                if (isBranch)
+                                    toggleExpand(element.id, isExpanded)
+                                else if (categoryEdit)
+                                    cancelCategoryEdit(categoryEdit.temp);
                             }
-                            {categoryEdit?.id !== element.id
-                                ? <>
-                                    <span className="name">{element.name}</span>
-                                    <div className="btns actions">
-                                        <ActionBtn
-                                            className={element.id < 0 ? 'hide' : ''}
+                        }),
+                        role: element.fixed ? 'header' : ''
+                    }}>
+                            <div className="tree-row">
+                                {
+                                    !element.fixed
+                                        ? isBranch
+                                            ? <ArrowIcon isOpen={isExpanded} />
+                                            : <ListIcon />
+                                        : <ActionBtn
+                                            className="icon-wrapper add-category"
                                             icon={faPlusCircle}
-                                            onClick={() => addNewCategoryToList(element.id, isExpanded)}
+                                            onClick={() => addNewCategoryToList(0, true)}
                                             tooltip={t('customButtons.add.tooltip')}
                                         />
-                                        <ActionBtn
-                                            className={element.id < 0 ? 'hide' : ''}
-                                            icon={faPen}
-                                            onClick={() => editCategory(element.id, element.name)}
-                                            tooltip={t('customButtons.edit.tooltip')}
+                                }
+                                {categoryEdit?.id !== element.id
+                                    ? <>
+                                        <span className="name">{element.name}</span>
+                                        <Actions
+                                            element={element}
+                                            isExpanded={isExpanded}
+                                            savingElements={savingElements}
+                                            addNewCategoryToList={addNewCategoryToList}
+                                            editCategory={editCategory}
+                                            deleteCategory={deleteCategory}
                                         />
-                                        <ActionBtn
-                                            className={element.id < 0 || element.children?.length > 0 ? 'hide' : ''}
-                                            icon={faTrash}
-                                            onClick={() => deleteCategory(element)}
-                                            tooltip={t('customButtons.delete.tooltip')}
-                                        />
+                                    </>
+                                    : <div className="edit-category">
+                                        <Form.Control
+                                            defaultValue={categoryEdit.name}
+                                            onBlur={e => {
+                                                if (e.currentTarget?.parentElement?.parentElement?.parentElement === e.relatedTarget) return;
+                                                cancelCategoryEdit(categoryEdit.temp);
+                                            }}
+                                            onChange={e => setCategoryEdit({ ...categoryEdit, name: e.currentTarget.value })}
+                                            onClick={e => e.stopPropagation()}
+                                            onKeyDown={e => {
+                                                e.stopPropagation();
+                                                if (e.code === "Enter") updateCategoryName(categoryEdit.temp);
+                                                if (e.code === "Escape") cancelCategoryEdit(categoryEdit.temp);
+                                            }}
+                                            name={`input-${element.id}`}
+                                            ref={inputRef}
+                                            type="text"
+                                            size="sm" />
+                                        <div className="btns">
+                                            <ActionBtn
+                                                // className={!categoryEdit.name ? 'hide' : ''}
+                                                icon={faCheckCircle}
+                                                onClick={() => updateCategoryName(categoryEdit.temp)}
+                                                tooltip={t('customButtons.confirm.tooltip')}
+                                            />
+                                        </div>
                                     </div>
-                                </>
-                                : <div className="edit-category">
-                                    <Form.Control
-                                        defaultValue={categoryEdit.name}
-                                        onBlur={e => {
-                                            if (e.currentTarget?.parentElement?.parentElement?.parentElement === e.relatedTarget) return;
-                                            cancelCategoryEdit(categoryEdit.temp);
-                                        }}
-                                        onChange={e => setCategoryEdit({ ...categoryEdit, name: e.currentTarget.value })}
-                                        onClick={e => e.stopPropagation()}
-                                        onKeyDown={e => {
-                                            e.stopPropagation();
-                                            if (e.code === "Enter") updateCategoryName(categoryEdit.temp);
-                                            if (e.code === "Escape") cancelCategoryEdit(categoryEdit.temp);
-                                        }}
-                                        name={`input-${element.id}`}
-                                        ref={inputRef}
-                                        type="text"
-                                        size="sm" />
-                                    <div className="btns">
-                                        <ActionBtn
-                                            // className={!categoryEdit.name ? 'hide' : ''}
-                                            icon={faCheckCircle}
-                                            onClick={() => updateCategoryName(categoryEdit.temp)}
-                                            tooltip={t('customButtons.confirm.tooltip')}
-                                        />
-                                    </div>
-                                </div>
-                            }
-                            {!element.fixed && !element.temp && element.id !== categoryEdit?.id &&
-                                <span className="doc-count">{element.numDocs || 0} docs</span>
-                            }
+                                }
+                                {!element.fixed && !element.temp && element.id !== categoryEdit?.id &&
+                                    <span className="doc-count">{element.numDocs || 0} docs</span>
+                                }
+                            </div>
                         </div>
-                    </div>
-                }
-            />
-
+                    }
+                />}
         </div>
     )
 }
