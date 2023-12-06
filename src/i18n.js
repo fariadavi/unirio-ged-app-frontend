b9757f7505f636ca78ccbd35a1372f47ec00eabb
Davi Faria <d.a.faria@gmail.com>
2023-12-06 01:52:50 -0300

    change default language

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,7 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
-        lng: 'en-US',
+        lng: 'pt-BR',
         keySeparator: false,
         interpolation: { escapeValue: false }
     });
